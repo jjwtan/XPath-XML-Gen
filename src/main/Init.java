783c3a651ac661f2f5968be8dcff971e015eeb12
Jeremy Tan <jjwtan@gmail.com>
2017-04-09 10:08:38 +0800

    remove init

--- src/main/Init.java
+++
@@ -1,8 +0,0 @@
-package main;
-
-public class Init {
-    public static void main(String[] args) {
-        XPathTreeBuilder xPath  = new XPathTreeBuilder("/scb:SCBML/scb:header/scb:captureSystem");
-        System.out.println("test " + xPath.getNodeTree().nodeSubPath());
-    }
-}
